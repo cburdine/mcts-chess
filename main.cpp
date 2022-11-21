722f8745bb061dd15d823936eef3985048cb1c39
cburdine <colin_burdine1@baylor.edu>
2022-11-20 18:30:40 -0600

    pushing training main function

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int main(){
     //self_play_instance.do_self_play_episode(1, dataset, cout, true);
     //save_chessnet_dataset(dataset, "chessnet_data.mcts");
     
+    //Perform training iterations on expert games:
     //PGNLoader loader = PGNLoader("data/games.pgn");
     //cout << "# games: " << loader.get_size() << endl;
 
@@ -31,14 +32,11 @@ int main(){
     //cout << dataset.size() << endl;
 
     //save_chessnet_dataset(dataset, "chessnet_data.mcts");
-    
     dataset.clear();
     load_chessnet_dataset(dataset, "chessnet_data.mcts");
-    //print_info(dataset, cout);
-
     self_play_instance.do_training_steps(1000, dataset, 12345, cout, true, true);
     self_play_instance.save_model();
-
+    
     /*
     // have the nnet play against itself:;
     auto game = ChessGame(
@@ -47,13 +45,13 @@ int main(){
     game.play();
     */
    
-    /*
+    
     // have a player (white) play against the nnet:
-    auto game = ChessGame(
-              make_unique<ChessPlayerAgent>(WHITE, cin), 
-              make_unique<ChessNetAgent>(BLACK, "./jupyter/simple_chess_net"));
-    game.play();
-    */
+    // auto game = ChessGame(
+    //           make_unique<ChessPlayerAgent>(WHITE, cin), 
+    //           make_unique<ChessNetAgent>(BLACK, "./jupyter/simple_chess_net"));
+    // game.play();
+    
 
     return 0;
 }
